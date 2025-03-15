a6b8b9049a5398200110e4b3dffafd997d9d224b
Alex Hedges <AJHedges@hotmail.com>
2025-03-15 19:32:11 +0000

    Added documentation

--- app/models.py
+++ app/models.py
@@ -1,7 +1,11 @@
 from sqlalchemy import Column, Integer, String
 from database import Base
 
+# This is the User model
 class User(Base):
+    """ This is the User model. 
+    It is a SQLAlchemy model that represents the users table in the database. """
+    
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
