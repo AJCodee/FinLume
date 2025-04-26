2ce5bc0d9255a355d12edaae54864689ee9acc02
Alex Hedges <AJHedges@hotmail.com>
2025-04-26 20:34:03 +0100

    Carried on adding functions to Bills Crud

--- app/crud/bills_crud.py
+++ app/crud/bills_crud.py
@@ -1,11 +1,11 @@
 from app.database import db_dependency
-from app.models import Bill
+from app.models import Bills
 from app.schemas.bills_schemas import BillCreate, BillUpdate
 
 class BillCRUD:
     """ This class will contain the CRUD Functions for the bill model. """
     
-    def create_new_bill(self, bill_data: BillCreate, db: db_dependency) -> Bill:
+    def create_new_bill(self, bill_data: BillCreate, db: db_dependency) -> Bills:
         """ Create a new bill in the database
         
         Args:
@@ -14,8 +14,13 @@ class BillCRUD:
             
         Response:
             Bill: The created bill object
+            
+        Raises:
+            ValueError if bill_data can not be validated.
         """
-        new_bill = Bill(
+        self._validate_bill(bill_data)
+        
+        new_bill = Bills(
             title = bill_data.title,
             amount = bill_data.amount,
             due_date = bill_data.due_date,
@@ -28,7 +33,7 @@ class BillCRUD:
         
         return new_bill
     
-    def update_bill(self, bill_id: int, bill_data: BillUpdate, db: db_dependency) -> Bill:
+    def update_bill(self, bill_id: int, bill_data: BillUpdate, db: db_dependency) -> Bills:
         """ Model for updating a Bill in the database 
         
         Args:
@@ -42,7 +47,7 @@ class BillCRUD:
             ValueError: Raises error if the exsisting Bill doesnt exsist in database
         """
         
-        exsisting_bill = get_bill_by_id(bill_id, db)
+        exsisting_bill = self.get_bill_by_id(bill_id, db)
         if not exsisting_bill:
             raise ValueError("Bill does not exsist.")
         
@@ -54,4 +59,25 @@ class BillCRUD:
         except Exception as e:
             db.rollback()
             raise Exception("Failed to update Bill") from e 
-            
+        
+    def _validate_bill(self, bill_data: BillCreate):
+        """ Function for checking is the created Bill is Valid """
+        
+        if not bill_data.title or len(bill_data.title) == 0:
+            raise ValueError("Title is required")
+        
+        if bill_data.amount <= 0:
+            raise ValueError("Amount is required and can not be 0")
+        
+    def get_all_bills(self, db: db_dependency) -> list[Bills]:
+        """ Used to retrieve all Bills in database """
+        return db.query(Bills).all()
+        
+    def get_bill_by_id(self, bill_id: int, db: db_dependency) -> Bills:
+        """ Used to retrieve a bill bu ID """
+        return db.query(Bills).filter(Bills.id == bill_id).first()
+    
+    def get_bill_per_user(self, user_id: int, db: db_dependency):
+        """ Retrives all the bills for a specific user """
+        return db.query(Bills).filter(Bills.user_id == Bills.user_id).all()
+    
