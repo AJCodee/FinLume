0810ba19f1c2f6b8f2c63371efa897ce4d9856b3
Alex Hedges <AJHedges@hotmail.com>
2025-04-29 22:31:38 +0100

    Begun work on bill_routes

--- app/routers/bills_routes.py
+++ app/routers/bills_routes.py
@@ -1,1 +1,5 @@
 # Routes for Bills.
+from fastapi import APIRouter, status
+from app.crud.bills_crud import BillCRUD
+from app.schemas.bills_schemas import BillCreate, BillUpdate
+from app.database import db_dependency
