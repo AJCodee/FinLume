0fcdfe220a8b7774ba6acff7fe97aa72ad141362
Alex Hedges <AJHedges@hotmail.com>
2025-05-11 20:25:57 +0100

    Added .env file for making database url safe

--- app/database.py
+++ app/database.py
@@ -3,9 +3,19 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from fastapi import Depends
 from typing import Annotated
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+# Load environment variables from .env file
+username = os.getenv("DB_USERNAME")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST")
+port = os.getenv("DB_PORT")
+database = os.getenv("DB_NAME")
 
 # Database URL, Connects to the database in postgres/PgAdmin
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:passpost@localhost:5432/FinLume"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{username}:{password}@{host}:{port}/{database}"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
