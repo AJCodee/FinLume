7c21cb10b6d1317be03e445ecca240ed2df5629d
Alex Hedges <AJHedges@hotmail.com>
2025-06-02 21:57:05 +0100

    Started work on auth_routes

--- app/routers/auth_routes.py
+++ app/routers/auth_routes.py
@@ -1,1 +1,6 @@
 # Used for authentication routes.
+from fastapi import APIRouter, Depends, HTTPException, status
+from app.crud.auth_crud import AuthCrud 
+from app.schemas.user_schemas import UserCreate, UserResponse
+from app.database import db_dependency
+from app.auth_utils import Token, create_access_token
