fb9bd2e2571b68c3ff279e9a50bdcdfd638805b1
Alex Hedges <AJHedges@hotmail.com>
2025-06-21 22:32:27 +0100

    Added first test to test_auth_routes

--- app/tests/test_routes/test_auth_crud.py
+++
@@ -1,4 +0,0 @@
-from app.database import Base, get_db
-from app.models import Users
-from app.crud import create_user, get_user_by_username, update_user, delete_user
-from app.tests.utils import *
---
+++ app/tests/test_routes/test_auth_routes.py
@@ -0,0 +1,20 @@
+from app.database import Base, get_db
+from app.models import Users
+from app.crud import create_user, get_user_by_username, update_user, delete_user
+from app.tests.utils import *
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+def test_register_user(test_user):
+    response = client.post("/auth/register",json={
+        "username": "newuser",
+        "first_name": "New",
+        "last_name": "User",
+        "email": "newuser@example.com",
+        "password": "newpassword"
+    })
+    assert response.status_code == 201
+    assert response.json()["username"] == "newuser"
+    
+    # Attempting to register a new user with the same username
