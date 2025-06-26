008480c298a72919ab0f16e42f64f094ea87da3b
Alex Hedges <AJHedges@hotmail.com>
2025-06-26 22:33:32 +0100

    Added comments to test_user routes

--- app/tests/test_routes/test_user_toutes.py
+++ app/tests/test_routes/test_user_toutes.py
@@ -6,17 +6,20 @@ from app.tests.utils import *
 app.dependency_overrides[get_db] = override_get_db
 app.dependency_overrides[get_current_user] = override_get_current_user
 
+# Testing returning all users
 def test_get_all_users(test_user):
     response = client.get("/users/return-users")
     assert response.status_code == 200
     assert isinstance(response.json(), list)
     assert len(response.json()) > 0
     
+# Testing returning with no users found
 def test_get_all_users_none():
     response = client.get("/users/return-users")
     assert response.status_code == 404
     assert response.json()['detail'] == "No users found"
     
+# Testing getting a user by ID
 def test_get_user_by_id(test_user):
     user_id = test_user.id # get the generated ID after commit
     response = client.get(f"/users/user-by-id/{user_id}")
@@ -26,6 +29,7 @@ def test_get_user_by_id(test_user):
     assert data['username'] == test_user.username
     assert data['email'] == test_user.email
     
+# Testing getting a user with an ID that doesnt match
 def test_get_user_by_id_not_found():
     non_exsisting_user_id = 9999
     response = client.get(F"/users/user-by-id/{non_exsisting_user_id}")
