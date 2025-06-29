b2170d62c9956909f0235868e7da4d0edf808754
Alex Hedges <AJHedges@hotmail.com>
2025-06-29 21:03:04 +0100

    Added more tests, need to work on test utils test_user_payment (payment format wrong)

--- app/tests/test_routes/test_user_toutes.py
+++ app/tests/test_routes/test_user_toutes.py
@@ -54,4 +54,14 @@ def test_get_user_by_username_not_found():
     
 # Testing returning all payments for a user using ID
 def test_get_user_payments(test_user_payment):
-     
+    user_id = test_user_payment.id
+    response = client.get(f"/users/all-payments/{user_id}")
+    assert response.status_code == 200
+    
+    data = response.json()
+    assert isinstance(data, list)
+    assert len(data) == 2
+    
+    # verify the contents of payments 
+    titles = [payment['title'] for payment in data]
+    assert "gas" in titles
