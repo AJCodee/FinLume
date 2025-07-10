1aa8079c6cc91e471c0f26baf2c7404b5bc43478
Alex Hedges <AJHedges@hotmail.com>
2025-07-10 22:47:45 +0100

    Began work on the bills routes test, Error on first text need to look into

--- app/tests/test_routes/test_bills_routes.py
+++ app/tests/test_routes/test_bills_routes.py
@@ -1,1 +1,16 @@
 # Testing the bills routes. 
+
+from app.database import get_db
+from app.tests.utils import *
+
+app.dependency_overrides[get_db] = override_get_db
+app.dependency_overrides[get_current_user] = override_get_current_user
+
+def test_create_new_bill(test_user):
+    response = client.post("/Bills/create",json={
+        "title": "newtitle",
+        "amount": 15,
+        "due_date": "2025-01-01",
+    })
+    assert response.status_code == 201
+    assert response.json()["title"] == "newtitle"
