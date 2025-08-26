b732b47d144bc844023a170884b9a27c4d6e9999
Alex Hedges <AJHedges@hotmail.com>
2025-08-26 19:23:45 +0100

    refac: created new user model

--- app/db/models/__init__.py
+++ app/db/models/__init__.py
@@ -1,2 +1,12 @@
 # Define one declarative base here. 
 # Importing the individaul models so Base.metadata sees all tables.
+
+from sqlalchemy.orm import declarative_base
+Base = declarative_base()
+
+from .user import User
+from .category import Category
+from .transaction import Transaction
+
+# Covenient export for Alembic
+metadata = Base.metadata
--- app/db/models/user.py
+++ app/db/models/user.py
@@ -1,2 +1,19 @@
 # class for User.
+from sqlalchemy.orm import declarative_base, relationship, Mapped, mapped_column
+from sqlalchemy import Integer, String, Boolean
+Base = declarative_base()
 
+# This is the User model
+class User(Base):
+    """ This is the User model. 
+    It is a SQLAlchemy model that represents the users table in the database. """
+    
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    first_name: Mapped[str] = mapped_column(String, nullable=False)
+    last_name: Mapped[str] = mapped_column(String, nullable=False)
+    username: Mapped[str] = mapped_column(String, unique=True, index=True)
+    email: Mapped[str] = mapped_column(String, unique=True, index=True)
+    hashed_password: Mapped[str] = mapped_column(String, nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
