c68f2fc4a03524a84952e57039607126dcd7595c
Alex Hedges <AJHedges@hotmail.com>
2025-10-02 21:20:04 +0100

    Refac: Started work on transaction CRUD

--- app/repositories/transaction_crud.py
+++ app/repositories/transaction_crud.py
@@ -1,1 +1,11 @@
 # Crud for transactions.
+
+from app.db.models import Transaction
+from app.schemas.transaction_schemas import TransactionCreate, TransactionUpdate
+from app.api.deps import DbDep
+
+class TransactionCrud:
+    """ This class will contain all the CRUD operations for the Transaction model. """
+    
+    def create_new_transaction(self, transaction_id: int, transaction: TransactionCreate, db: DbDep):
+        """ This method will create and return a new transaction. """
